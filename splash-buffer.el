a5f41d19854002ef58aae1884b7ac4cd3f54e89d
Aaron Jacobs <atheriel@gmail.com>
2015-05-20 17:12:06 -0400

    Adds the basic package structure and metadata.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

---
+++ splash-buffer.el
@@ -0,0 +1,37 @@
+;;; splash-buffer.el --- Maintain a custom splash/start-up buffer.
+
+;; Copyright (C) 2015 Aaron Jacobs
+
+;; Author: Aaron Jacobs <atheriel@gmail.com>
+;; Version: 0.1
+;; Keywords: help
+;; URL: https://github.com/atheriel/splash-buffer.el
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; https://github.com/atheriel/splash-buffer.el
+
+;;; Code:
+
+(defgroup splash-buffer nil
+  "Maintain a custom splash/start-up buffer."
+  :group 'convenience)
+
+(provide 'splash-buffer)
+
+;;; splash-buffer.el ends here
