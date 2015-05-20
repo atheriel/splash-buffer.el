9a90e7d590295cacb4a0d45ca2ef2abe10a96bf6
Aaron Jacobs <atheriel@gmail.com>
2015-05-20 17:32:41 -0400

    Adds a basic `splash-buffer-show' function.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- splash-buffer.el
+++ splash-buffer.el
@@ -32,6 +32,47 @@
   "Maintain a custom splash/start-up buffer."
   :group 'convenience)
 
+(defcustom splash-buffer-buffer-name "*GNU Emacs*"
+  "The name of the buffer used by splash-buffer."
+  :group 'splash-buffer
+  :version 0.1
+  :type 'string)
+
+(defcustom splash-buffer-contents fancy-startup-text
+  "The contents of the splash buffer."
+  :group 'splash-buffer
+  :version 0.1)
+
+(defun splash-buffer-show ()
+  "Creates or updates the splash buffer, then switches to it.
+
+This is a modifed version of the built-in `fancy-startup-screen'
+function from `startup.el'."
+  (let ((buff (get-buffer-create splash-buffer-buffer-name)))
+    (with-current-buffer buff
+      ;; Update the contents of the splash buffer.
+      (let ((inhibit-read-only t))
+	(erase-buffer)
+	(fancy-splash-head)
+	(dolist (text splash-buffer-contents)
+	  (apply #'fancy-splash-insert text)
+	  (insert "\n"))
+	(skip-chars-backward "\n")
+	(delete-region (point) (point-max))
+	(insert "\n")
+	(fancy-startup-tail))
+      ;; Apply local settings.
+      (use-local-map splash-screen-keymap)
+      (setq-local browse-url-browser-function 'eww-browse-url)
+      (setq tab-width 22
+	    buffer-read-only t)
+      (set-buffer-modified-p nil)
+      ;; Position the cursor.
+      (goto-char (point-min))
+      (forward-line 4))
+    ;; Finally, switch to the buffer.
+    (switch-to-buffer buff)))
+
 (provide 'splash-buffer)
 
 ;;; splash-buffer.el ends here
